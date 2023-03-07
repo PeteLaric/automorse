2ad6dc0f35dd54aeb49b58769e62bb330f1f528e
PeteLaric <52544596+PeteLaric@users.noreply.github.com>
2023-03-07 05:29:38 -0700

    Update fade.m

--- fade.m
+++ fade.m
@@ -6,4 +6,4 @@ function v = fade (v, fade_in_samples, fade_out_samples)
     % fade out
     fade_out_amplitudes = linspace(1, 0, fade_out_samples);
     v(end-fade_out_samples+1:end) = v(end-fade_out_samples+1:end) .* fade_out_amplitudes;
-endfunction
+end
